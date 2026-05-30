b727d2a3d5b43623a27ef13cd48dfd5c640d2d4e
Luwi Musole <mluwi46@gmail.com>
2026-05-30 13:14:14 +0800

    Added new patient dashboard feature

--- src/PatientApp.java
+++ src/PatientApp.java
@@ -1,12 +1,5 @@
-import javax.swing.*;
-
 public class PatientApp {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SmartMed");
-        JLabel label = new JLabel("Welcome to SmartMed Patient System", JLabel.CENTER);
-        frame.add(label);
-        frame.setSize(400, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        System.out.println("Welcome to SmartMed Patient System");
     }
 }
